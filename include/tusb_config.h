571e3e4835a3559e3c45869d12c8627cd15e2616
HASUMI Hitoshi <hasumikin@gmail.com>
2023-04-13 16:39:34 +0900

    Fix serial terminal wrap bug

--- include/tusb_config.h
+++ include/tusb_config.h
@@ -105,11 +105,11 @@
 #define CFG_TUD_VENDOR           0
 
 // CDC FIFO size of TX and RX
-#define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
-#define CFG_TUD_CDC_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_RX_BUFSIZE    512
+#define CFG_TUD_CDC_TX_BUFSIZE    512
 
 // CDC Endpoint transfer buffer size, more is faster
-#define CFG_TUD_CDC_EP_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_EP_BUFSIZE    512
 
 // MSC Buffer size of Device Mass storage
 #if defined(PICORUBY_MSC_FLASH)
