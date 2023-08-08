3a0e18c831a14a57282789494ae9454e2797477b
HASUMI Hitoshi <hasumikin@gmail.com>
2023-08-08 17:17:40 +0900

    improve "Skip running app"

--- mrblib/r2p2_task.rb
+++ mrblib/r2p2_task.rb
@@ -20,11 +20,24 @@ end
 
 $reboot_count = 0
 begin
-  # Execute /home/app.mrb or /home/app.rb
-  if File.exist?("/home/app.mrb")
-    Shell::Command.new.exec("/home/app.mrb")
-  elsif File.exist?("/home/app.rb")
-    Shell::Command.new.exec("/home/app.rb")
+  puts "Press 's' to skip running app.mrb or app.rb"
+  skip = false
+  10.times do
+    if IO.getch == "s"
+      puts "Skip running app"
+      skip = true
+      break
+    end
+    sleep 0.1
+  end
+  IO.read_nonblock 1024 # discard remaining input
+  unless skip
+    # Execute /home/app.mrb or /home/app.rb
+    if File.exist?("/home/app.mrb")
+      Shell::Command.new.exec("/home/app.mrb")
+    elsif File.exist?("/home/app.rb")
+      Shell::Command.new.exec("/home/app.rb")
+    end
   end
   # Start shell if terminal is available
   IO.wait_terminal
