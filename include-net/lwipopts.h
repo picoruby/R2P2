b5b761699eab593eb6ee96746e02f1b1f5858805
Ryo Kajiwara <sylph01@s01.ninja>
2024-08-26 21:35:31 +0900

    Styling fix / remove comments from lwipopts.h

--- include-net/lwipopts.h
+++ include-net/lwipopts.h
@@ -8,16 +8,19 @@
 #ifndef NO_SYS
 #define NO_SYS 1
 #endif
+
 // allow override in some examples
 #ifndef LWIP_SOCKET
 #define LWIP_SOCKET 0
 #endif
+
 #if PICO_CYW43_ARCH_POLL
 #define MEM_LIBC_MALLOC 1
 #else
 // MEM_LIBC_MALLOC is incompatible with non polling versions
 #define MEM_LIBC_MALLOC 0
 #endif
+
 #define MEM_ALIGNMENT 4
 #define MEM_SIZE 4800
 #define MEMP_NUM_TCP_SEG 32
@@ -39,7 +42,6 @@
 #define SYS_STATS 0
 #define MEMP_STATS 0
 #define LINK_STATS 0
-// #define ETH_PAD_SIZE 2
 #define LWIP_CHKSUM_ALGORITHM 3
 #define LWIP_DHCP 1
 #define LWIP_IPV4 1
@@ -51,45 +53,8 @@
 #define DHCP_DOES_ARP_CHECK 0
 #define LWIP_DHCP_DOES_ACD_CHECK 0
 
-/*
-#ifndef NDEBUG
-#define LWIP_DEBUG 1
-#define LWIP_STATS 1
-#define LWIP_STATS_DISPLAY 1
-#endif
-
-#define ETHARP_DEBUG LWIP_DBG_OFF
-#define NETIF_DEBUG LWIP_DBG_OFF
-#define PBUF_DEBUG LWIP_DBG_OFF
-#define API_LIB_DEBUG LWIP_DBG_OFF
-#define API_MSG_DEBUG LWIP_DBG_OFF
-#define SOCKETS_DEBUG LWIP_DBG_OFF
-#define ICMP_DEBUG LWIP_DBG_OFF
-#define INET_DEBUG LWIP_DBG_OFF
-#define IP_DEBUG LWIP_DBG_OFF
-#define IP_REASS_DEBUG LWIP_DBG_OFF
-#define RAW_DEBUG LWIP_DBG_OFF
-#define MEM_DEBUG LWIP_DBG_OFF
-#define MEMP_DEBUG LWIP_DBG_OFF
-#define SYS_DEBUG LWIP_DBG_OFF
-#define TCP_DEBUG LWIP_DBG_OFF
-#define TCP_INPUT_DEBUG LWIP_DBG_OFF
-#define TCP_OUTPUT_DEBUG LWIP_DBG_OFF
-#define TCP_RTO_DEBUG LWIP_DBG_OFF
-#define TCP_CWND_DEBUG LWIP_DBG_OFF
-#define TCP_WND_DEBUG LWIP_DBG_OFF
-#define TCP_FR_DEBUG LWIP_DBG_OFF
-#define TCP_QLEN_DEBUG LWIP_DBG_OFF
-#define TCP_RST_DEBUG LWIP_DBG_OFF
-#define UDP_DEBUG LWIP_DBG_OFF
-#define TCPIP_DEBUG LWIP_DBG_OFF
-#define PPP_DEBUG LWIP_DBG_OFF
-#define SLIP_DEBUG LWIP_DBG_OFF
-#define DHCP_DEBUG LWIP_DBG_OFF
-*/
-
- #define LWIP_ALTCP 1
- #define LWIP_ALTCP_TLS 1
- #define LWIP_ALTCP_TLS_MBEDTLS 1
+#define LWIP_ALTCP 1
+#define LWIP_ALTCP_TLS 1
+#define LWIP_ALTCP_TLS_MBEDTLS 1
 
 #endif /* __LWIPOPTS_H__ */
