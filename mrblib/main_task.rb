580b2cf85d46a4675df679a2467569c82e6d8ce9
HASUMI Hitoshi <hasumikin@gmail.com>
2025-04-23 11:46:15 +0900

    backport from MicroRuby

--- mrblib/main_task.rb
+++ mrblib/main_task.rb
@@ -12,13 +12,6 @@ begin
   Shell.setup_root_volume(:flash, label: "R2P2")
   Shell.setup_system_files
 
-  begin
-    require "cyw43"
-    ENV['WIFI_MODULE'] = "cwy43"
-  rescue LoadError
-    # No WiFi module
-  end
-
   Shell.bootstrap("/etc/init.d/r2p2")
 
   $shell = Shell.new(clean: true)
